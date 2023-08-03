564fc3df31b4faecedd141a2551abf1d8dd29fdb
mraf19 <raflimr1902@gmail.com>
2023-08-03 09:43:32 +0700

    fix some bug

--- app/player/router.js
+++ app/player/router.js
@@ -1,21 +1,32 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { editProfile, profile, dashboard, landingPage, detailPage, category, checkout, history, historyDetail } = require('./controller')
-const { isLoginPlayer } = require('../middleware/auth')
-const multer = require('multer')
-const os = require('os')
+const {
+  editProfile,
+  profile,
+  dashboard,
+  landingPage,
+  detailPage,
+  category,
+  checkout,
+  history,
+  historyDetail,
+} = require("./controller");
+const { isLoginPlayer } = require("../middleware/auth");
+const multer = require("multer");
+const os = require("os");
 
-router.get('/landingpage', landingPage);
-router.get('/:id/detail', detailPage);
-router.get('/category', category);
-router.post('/checkout', isLoginPlayer, checkout);
-router.get('/history', isLoginPlayer, history);
-router.get('/history/:id/detail', isLoginPlayer, historyDetail);
-router.get('/dashboard', isLoginPlayer, dashboard);
-router.get('/profile', isLoginPlayer, profile);
-router.put('/profile',
+router.get("/landingpage", landingPage);
+router.get("/:id/detail", detailPage);
+router.get("/category", category);
+router.post("/checkout", isLoginPlayer, checkout);
+router.get("/history", isLoginPlayer, history);
+router.get("/history/:id/detail", isLoginPlayer, historyDetail);
+router.get("/dashboard", isLoginPlayer, dashboard);
+router.get("/profile", isLoginPlayer, profile);
+router.put(
+  "/profile/:id",
   isLoginPlayer,
-  multer({ dest: os.tmpdir() }).single('image'),
+  multer({ dest: os.tmpdir() }).single("image"),
   editProfile
 );
 
